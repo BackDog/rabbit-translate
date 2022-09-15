4b920ec8a39d3852ee3b6a5728ea541ae3c12ab6
Thanh Cảnh Nguyễn <canhnt1204@gmail.com>
2022-09-16 04:38:27 +0700

    4
    4

--- index.js
+++ index.js
@@ -5,35 +5,7 @@ const app = express();
 
 console.log('Starting Server....');
 
-(async () => {
-    const browser = await chromium.launch();
-    const context = await browser.newContext();
-
-    app.get('/translate', function (req, res) {
-        console.log('Translate ' + req.query.text);
-        translate(req.query.text, res);
-    })
-
-    async function translate(text, res) {
-        try{
-            const googleTranslate = await context.newPage();
-            await googleTranslate.goto('https://translate.google.com.vn/?hl=vi&sl=vi&tl=en&op=translate');
-            await googleTranslate.waitForLoadState('networkidle');
-            await googleTranslate.locator('textarea[aria-label="Văn bản nguồn"]').fill(text);
-            await googleTranslate.waitForLoadState('networkidle');
-            while(true) {
-                let output = await googleTranslate.locator('span[data-language-to-translate-into="vi"]').innerText();
-                if (output != LAST_TRANS_OUTPUT){
-                    LAST_TRANS_OUTPUT = output;
-                    break;
-                }else{
-                    await googleTranslate.waitForTimeout(100);
-                }
-            }
-            res.send(LAST_TRANS_OUTPUT);
-            await browser.close();
-        } catch (error) {
-            console.log(error);
-        }
-    }
-})();
+app.get('/translate', function (req, res) {
+    console.log('Translate ' + req.query.text);
+    res.send(req.query.text);
+})
