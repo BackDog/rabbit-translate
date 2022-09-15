003cebf1c29c7882622f56a883a8639e363bf886
Thanh Cảnh Nguyễn <canhnt1204@gmail.com>
2022-09-16 04:24:37 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,29 +3,36 @@ const { webkit } = require('playwright');
 
 const app = express();
 
+console.log('Starting Server...');
+
 (async () => {
     const browser = await webkit.launch();
 
     app.get('/translate', function (req, res) {
+        console.log('Translate ' + req.query.text);
         translate(req.query.text, res);
     })
 
     async function translate(text, res) {
-        const googleTranslate = await browser.newPage();
-        await googleTranslate.goto('https://translate.google.com.vn/?hl=vi&sl=vi&tl=en&op=translate');
-        await googleTranslate.waitForLoadState('networkidle');
-        await googleTranslate.locator('textarea[aria-label="Văn bản nguồn"]').fill(text);
-        await googleTranslate.waitForLoadState('networkidle');
-        while(true) {
-            let output = await googleTranslate.locator('span[data-language-to-translate-into="vi"]').innerText();
-            if (output != LAST_TRANS_OUTPUT){
-                LAST_TRANS_OUTPUT = output;
-                break;
-            }else{
-                await googleTranslate.waitForTimeout(100);
+        try{
+            const googleTranslate = await browser.newPage();
+            await googleTranslate.goto('https://translate.google.com.vn/?hl=vi&sl=vi&tl=en&op=translate');
+            await googleTranslate.waitForLoadState('networkidle');
+            await googleTranslate.locator('textarea[aria-label="Văn bản nguồn"]').fill(text);
+            await googleTranslate.waitForLoadState('networkidle');
+            while(true) {
+                let output = await googleTranslate.locator('span[data-language-to-translate-into="vi"]').innerText();
+                if (output != LAST_TRANS_OUTPUT){
+                    LAST_TRANS_OUTPUT = output;
+                    break;
+                }else{
+                    await googleTranslate.waitForTimeout(100);
+                }
             }
+            res.send(LAST_TRANS_OUTPUT);
+            await browser.close();
+        } catch (error) {
+            console.log(error);
         }
-        res.send(LAST_TRANS_OUTPUT);
-        await browser.close();
     }
 })();
